90be47e5959702da91903b8d20e2c71561becdc7
Kristofor Maynard <kristofor.maynard@gmail.com>
2019-04-19 23:05:27 -0400

    bump to 0.0.2

--- wren_tag/__init__.py
+++ wren_tag/__init__.py
@@ -23,7 +23,7 @@ from wren_tag import vorbis
 from wren_tag.file import Artwork, MetadataItem, NotAppendable, AudioFile
 
 
-__version__ = """0.0.1"""
+__version__ = """0.0.2"""
 
 
 logger = logging.getLogger("wren")
